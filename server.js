96d6e60df0c2d91f45d2ec8f3d40efa5fae5eeb3
Marra <marrabarbosa9@gmail.com>
2024-09-19 08:58:52 -0300

    Integração com banco de dados SQLite

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000; // Defina a porta do servidor
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
- });
- 
- app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
- });
+app.use(express.json());
+
+// Cria um banco de dados SQLite em memória
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('lista-tarefas.db');
+
+// Cria a tabela 'tarefas' no banco de dados
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
+});
+
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
