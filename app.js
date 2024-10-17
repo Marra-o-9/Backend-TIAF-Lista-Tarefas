3e3f9deb1aef14ff8a01eac4735086aa4bf30d11
Marra <marrabarbosa9@gmail.com>
2024-10-17 09:06:53 -0300

    Integração do CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -6,6 +7,9 @@ const authMiddleware = require('./middlewares/authMiddleware');
 
 // Middleware para JSON
 app.use(express.json());
+
+// Inicializa o CORS
+app.use(cors()); // Aqui é onde você deve inicializar o CORS
 
 // Rotas
 app.use('/api', userRoutes); // Rota para autenticação
